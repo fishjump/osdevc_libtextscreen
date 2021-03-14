c037b60b9ec9ed752fac3fffa810566e66e5546d
fishjump <z1052525203@outlook.com>
2021-03-14 22:04:25 +0800

    workround for unknown github workflow issue

--- include/text_screen/text_screen.hpp
+++ include/text_screen/text_screen.hpp
@@ -1,3 +1,29 @@
 #pragma once
 
-#include "entity.hpp"
+#include <screen/screen.hpp>
+#include <std/stdcxx.hpp>
+
+namespace system::io::entity
+{
+    class TextScreen : public Screen
+    {
+    public:
+        TextScreen();
+
+        uint32_t row;
+        uint32_t column;
+        static const uint32_t MAX_BUFFER_SIZE = 4096;
+        char buffer[MAX_BUFFER_SIZE];
+
+        void drawChar(uint32_t x, uint32_t y, system::media::entity::Color color, char ch);
+
+        TextScreen *print(const char *content);
+        TextScreen *print(int64_t content);
+        TextScreen *print(uint64_t content);
+
+        void fresh();
+    private:
+        uint32_t bufferCount = 0;
+    };
+
+} // namespace system::io::entity
