fe6aa0fafa0105d3ce2ff09bede7d32fc00fe1b9
fishjump <z1052525203@outlook.com>
2021-03-20 19:50:46 +0800

    fit font update

--- include/text_screen/entity.hpp
+++ include/text_screen/entity.hpp
@@ -1,30 +1,41 @@
 #pragma once
 
+#include <color/color.hpp>
+#include <font/font.hpp>
 #include <screen/screen.hpp>
 #include <std/stdcxx.hpp>
 
-namespace system::io::entity
-{
-    class TextScreen : public Screen
-    {
-    public:
-        TextScreen();
+namespace system::io::entity {
 
-        uint32_t row;
-        uint32_t column;
-        static const uint32_t MAX_BUFFER_SIZE = 4096;
-        char buffer[MAX_BUFFER_SIZE];
+class TextScreen : public Screen {
+public:
+    TextScreen(system::media::entity::Color defaultColor,
+               system::media::entity::Font  defaultFont);
 
-        void drawChar(uint32_t x, uint32_t y, system::media::entity::Color color, char ch);
+    static const uint32_t MAX_BUFFER_SIZE = 4096;
 
-        TextScreen *print(const char *content);
-        TextScreen *print(int64_t content);
-        TextScreen *print(uint64_t content);
+    TextScreen *drawChar(const uint32_t &x, const uint32_t &y,
+                         const uint8_t &ch);
 
-        void fresh();
+    TextScreen *drawChar(const uint32_t &x, const uint32_t &y,
+                         const uint8_t &                     ch,
+                         const system::media::entity::Color &color,
+                         const system::media::entity::Font & font);
 
-    private:
-        uint32_t bufferCount = 0;
-    };
+    TextScreen *print(const uint8_t *content);
+    TextScreen *print(int64_t content);
+    TextScreen *print(uint64_t content);
+
+    void fresh();
+
+private:
+    char     buffer[MAX_BUFFER_SIZE];
+    uint32_t bufferCount;
+    uint32_t row;
+    uint32_t column;
+
+    system::media::entity::Color defaultColor;
+    system::media::entity::Font  defaultFont;
+}; // class TextScreen
 
 } // namespace system::io::entity
--- src/text_screen.cpp
+++ src/text_screen.cpp
@@ -1,169 +1,156 @@
+#include <color/color.hpp>
 #include <font/font.hpp>
 #include <text_screen/text_screen.hpp>
 
-namespace system::io
-{
-    entity::TextScreen::TextScreen() : row(height / system::media::entity::Font::fontHeight),
-                                       column(width / system::media::entity::Font::fontWidth) {}
+namespace system::io {
 
-    void entity::TextScreen::drawChar(unsigned x, unsigned y, system::media::entity::Color color, char ch)
-    {
-        system::media::entity::Font font(ch);
-        system::media::entity::Font::FontRows fontRows = font.getFontRows();
+entity::TextScreen::TextScreen(system::media::entity::Color defaultColor,
+                               system::media::entity::Font  defaultFont)
+    : defaultColor(defaultColor), defaultFont(defaultFont),
+      row(height / system::media::entity::Font::fontHeight),
+      column(width / system::media::entity::Font::fontWidth) {}
 
-        unsigned xBoundary = x + system::media::entity::Font::fontWidth, yBoundary = y + system::media::entity::Font::fontHeight;
-        for (unsigned _y = y, fontRowIndex = 0; _y < yBoundary; _y++, fontRowIndex++)
-        {
-            char fontRow = fontRows[fontRowIndex];
-            for (unsigned _x = x; _x < xBoundary; _x++)
-            {
-                if (fontRow & 0b10000000)
-                {
-                    drawPixel(_x, _y, color);
-                }
-                else
-                {
-                    drawPixel(_x, _y, system::media::common_color::black);
-                }
+entity::TextScreen *
+entity::TextScreen::drawChar(const uint32_t &x, const uint32_t &y,
+                             const uint8_t &                     ch,
+                             const system::media::entity::Color &color,
+                             const system::media::entity::Font & font) {
 
-                fontRow = fontRow << 1;
+    const uint8_t *charMap = font.getCharMap(ch);
+
+    uint32_t xBoundary = x + system::media::entity::Font::fontWidth,
+             yBoundary = y + system::media::entity::Font::fontHeight;
+
+    for(uint32_t _y = y, fontMapRowIndex = 0; _y < yBoundary;
+        _y++, fontMapRowIndex++) {
+        uint8_t fontMapRow = charMap[fontMapRowIndex];
+        for(uint32_t _x = x; _x < xBoundary; _x++) {
+            if(fontMapRow & 0b10000000) {
+                drawPixel(_x, _y, color);
             }
+            fontMapRow = fontMapRow << 1;
         }
     }
 
-    entity::TextScreen *entity::TextScreen::print(const char *content)
-    {
-        int i = 0;
-        while (content[i] != '\0')
-        {
-            buffer[bufferCount] = content[i];
-            bufferCount++;
-            i++;
-        }
+    return this;
+}
 
-        return this;
+entity::TextScreen *entity::TextScreen::print(const uint8_t *content) {
+    int i = 0;
+    while(content[i] != '\0') {
+        buffer[bufferCount] = content[i];
+        bufferCount++;
+        i++;
     }
 
-    entity::TextScreen *entity::TextScreen::print(int64_t content)
-    {
-        char arr[64] = {}; // length is long enought now
+    return this;
+}
 
-        if (content == 0)
-        {
-            arr[0] = '0';
-            return print(arr);
-        }
+entity::TextScreen *entity::TextScreen::print(int64_t content) {
+    uint8_t arr[64] = {}; // length is long enought now
 
-        int64_t tmp = content, length = 0;
-        while (tmp != 0)
-        {
-            tmp /= 10;
-            length++;
-        }
-
-        int index = length - 1, end = 0;
-        if (content < 0)
-        {
-            arr[0] = '-';
-            content = -content;
-            index++;
-            end++;
-        }
-
-        while (index >= end)
-        {
-            arr[index] = '0' + content % 10;
-            content /= 10;
-            index--;
-        }
-
+    if(content == 0) {
+        arr[0] = '0';
         return print(arr);
     }
 
-    entity::TextScreen *entity::TextScreen::print(uint64_t content)
-    {
-        char arr[64] = {}; // length is long enought now
+    int64_t tmp = content, length = 0;
+    while(tmp != 0) {
+        tmp /= 10;
+        length++;
+    }
 
-        if (content == 0)
-        {
-            arr[0] = '0';
-            return print(arr);
-        }
+    int index = length - 1, end = 0;
+    if(content < 0) {
+        arr[0]  = '-';
+        content = -content;
+        index++;
+        end++;
+    }
 
-        uint64_t tmp = content, length = 0;
-        while (tmp != 0)
-        {
-            tmp /= 10;
-            length++;
-        }
+    while(index >= end) {
+        arr[index] = '0' + content % 10;
+        content /= 10;
+        index--;
+    }
 
-        int index = length - 1, end = 0;
-        if (content < 0)
-        {
-            arr[0] = '-';
-            content = -content;
-            index++;
-            end++;
-        }
+    return print(arr);
+}
 
-        while (index >= end)
-        {
-            arr[index] = '0' + content % 10;
-            content /= 10;
-            index--;
-        }
+entity::TextScreen *entity::TextScreen::print(uint64_t content) {
+    uint8_t arr[64] = {}; // length is long enought now
 
+    if(content == 0) {
+        arr[0] = '0';
         return print(arr);
     }
 
-    void entity::TextScreen::fresh()
-    {
-        int cursor = 0;
-        for (int i = 0; i < bufferCount; i++)
-        {
-            if (cursor >= row * column)
-            {
-                clear();
-                cursor = 0;
-            }
-            switch (buffer[i])
-            {
-            case '\n':
-            {
+    uint64_t tmp = content, length = 0;
+    while(tmp != 0) {
+        tmp /= 10;
+        length++;
+    }
+
+    int index = length - 1, end = 0;
+    if(content < 0) {
+        arr[0]  = '-';
+        content = -content;
+        index++;
+        end++;
+    }
+
+    while(index >= end) {
+        arr[index] = '0' + content % 10;
+        content /= 10;
+        index--;
+    }
+
+    return print(arr);
+}
+
+void entity::TextScreen::fresh() {
+    int cursor = 0;
+    for(int i = 0; i < bufferCount; i++) {
+        if(cursor >= row * column) {
+            clear();
+            cursor = 0;
+        }
+        switch(buffer[i]) {
+            case '\n': {
                 cursor += column - cursor % column;
                 break;
             }
-            case '\r':
-            {
+            case '\r': {
                 cursor += column - cursor % column;
                 break;
             }
-            case '\b':
-            {
-                if (cursor > 0)
-                {
-                    int x = (cursor % column) * system::media::entity::Font::fontWidth;
-                    int y = (cursor / column) * system::media::entity::Font::fontHeight;
-                    drawChar(x, y, system::media::common_color::white, ' ');
+            case '\b': {
+                if(cursor > 0) {
+                    int x = (cursor % column)
+                            * system::media::entity::Font::fontWidth;
+                    int y = (cursor / column)
+                            * system::media::entity::Font::fontHeight;
+                    drawChar(x, y, ' ', defaultColor, defaultFont);
                     cursor--;
                 }
                 break;
             }
-            default:
-            {
-                int x = (cursor % column) * system::media::entity::Font::fontWidth;
-                int y = (cursor / column) * system::media::entity::Font::fontHeight;
-                drawChar(x, y, system::media::common_color::white, buffer[i]);
+            default: {
+                int x = (cursor % column)
+                        * system::media::entity::Font::fontWidth;
+                int y = (cursor / column)
+                        * system::media::entity::Font::fontHeight;
+                drawChar(x, y, buffer[i], defaultColor, defaultFont);
                 cursor++;
                 break;
             }
-            }
         }
-
-        // draw cursor
-        int x = (cursor % column) * system::media::entity::Font::fontWidth;
-        int y = (cursor / column) * system::media::entity::Font::fontHeight;
-        drawChar(x, y, system::media::common_color::white, '_');
     }
+
+    // draw cursor
+    int x = (cursor % column) * system::media::entity::Font::fontWidth;
+    int y = (cursor / column) * system::media::entity::Font::fontHeight;
+    drawChar(x, y, '_', defaultColor, defaultFont);
+}
 
 } // namespace system::io
